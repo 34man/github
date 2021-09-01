5901501e8baf20c9c09f49fb21efb7f67c715474
34man <42126425+34man@users.noreply.github.com>
2021-09-01 21:28:48 +0800

    tijao

--- leetcode/number_list.py
+++ leetcode/number_list.py
@@ -184,12 +184,50 @@ class Solution:
                     dp[i] = dp[0] + num
         return dp[-1]
 
+一些恶魔抓住了公主（P）并将她关在了地下城的右下角。地下城是由 M x N 个房间组成的二维网格。
+我们英勇的骑士（K）最初被安置在左上角的房间里，他必须穿过地下城并通过对抗恶魔来拯救公主。
+
+骑士的初始健康点数为一个正整数。如果他的健康点数在某一时刻降至 0 或以下，他会立即死亡。
+
+有些房间由恶魔守卫，因此骑士在进入这些房间时会失去健康点数（若房间里的值为负整数，则表示骑士将损失健康点数）；
+其他房间要么是空的（房间里的值为 0），要么包含增加骑士健康点数的魔法球（若房间里的值为正整数，则表示骑士将增加健康点数）。
+
+为了尽快到达公主，骑士决定每次只向右或向下移动一步。
+
+class Solution:
+    def calculateMinimumHP(self, dungeon):
+        dp = [[sum(dungeon[0])+1, 0] for _ in range(len(dungeon[0]))]
+        dp[0][0] = dungeon[0][0]
+        dp[0][1] = dungeon[0][0]
+
+        for i, eles in enumerate(dungeon):
+            for j, ele in enumerate(eles):
+                print(dp)
+                if i == 0:
+                    if j > 0:
+                        dp[j][0] = min(dp[j-1][0]+ele, dp[j-1][0])
+                        dp[j][1] = dp[j-1][1]+ele
+                else:
+                    if j > 0:
+                        tip1 = 0 if ele+dp[j-1][0]+dp[j-1][1]>0 else ele+dp[j-1][0]+dp[j-1][1]
+                        tip2 = 0 if ele+dp[j][0]+dp[j-1][1]>0 else ele+dp[j][0]+dp[j-1][1]
+                        dp[j][0] = min(max(tip1, tip2), dp[i-1][0])
+                        dp[j][1] = max(ele+dp[j-1][1], dp[j][1]+ele)
+                    else:
+                        dp[j][0] = min(dp[j][0]+ele, dp[j][0])
+                        dp[j][1] = dp[j][1]+ele
+        print(dp)
+        return 0 if dp[0][0]>0 else -dp[0][0]
+s = Solution()
+s.calculateMinimumHP([[-2,-3,3],[-5,-10,1],[10,30,-5]])
 
 
+-2 -3  3
+-5 -10 1
+10 30 -5
 
 
-
-
+[[1, 0]]*2
 
 
 pass
